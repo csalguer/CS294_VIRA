9a1c321f6eb99c938342db795ac3b6967f51fc77
iakujobi16 <iakujobi16@gmail.com>
2017-05-31 07:09:17 -0700

    Basic Working alarm clock. Call start_alarm from other files to start.

--- vira/alarm_util.py
+++ vira/alarm_util.py
@@ -8,5 +8,67 @@ CS294W, Spring 2016-2017.
 © Stanford University.
 """
 
+import datetime
+import sys
+import threading
+import subprocess
+import multiprocessing
+
+
 class AlarmUtility(object):
     """A basic alarm clock"""
+
+    def __init__(self):
+        self.alarm_time = None
+        self._alarm_thread = None
+        self.update_interval = 1
+        self.event = threading.Event()
+
+    def run(self):
+        while True:
+            self.event.wait(self.update_interval)
+            if self.event.isSet():
+                break
+            now = datetime.datetime.now()
+            if self._alarm_thread and self._alarm_thread.is_alive():
+                alarm_symbol = '+'
+            else:
+                alarm_symbol = ' '
+            # sys.stdout.write("\r%02d:%02d:%02d %s" 
+            #     % (now.hour, now.minute, now.second, alarm_symbol))
+            sys.stdout.flush()
+
+    def ring(self):
+    	self.event.set()
+    	path = '/Applications/VLC.app/Contents/MacOS/VLC'
+    	mp3 = '/Users/Ikechi/Downloads/01sispuellamagica.mp3'
+    	subprocess.call([path, mp3])
+
+
+    def set_alarm(self, hour, minute):
+        now = datetime.datetime.now()
+        alarm = now.replace(hour=int(hour), minute=int(minute), second = 0)
+        delta = int((alarm - now).total_seconds())
+        if delta <= 0:
+            alarm = alarm.replace(day=alarm.day + 1)
+            delta = int((alarm - now).total_seconds())
+        if self._alarm_thread:
+            self._alarm_thread.cancel()
+        self._alarm_thread = threading.Timer(delta, self.ring)
+        self._alarm_thread.daemon = True
+        self._alarm_thread.start()
+
+
+def process_func(hour, minute):
+	alarm = AlarmUtility()
+	alarm.set_alarm(hour, minute)
+	alarm.run()
+
+def start_alarm():
+	hour = "10"
+	minute = "10"
+	p = multiprocessing.Process(target=main, args=(hour, minute))
+	p.start()
+
+if __name__ == "__main__":
+	
