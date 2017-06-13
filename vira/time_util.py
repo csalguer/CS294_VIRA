20bd00725251551d0733908a7d35cc5670621944
iakujobi16 <iakujobi16@gmail.com>
2017-06-12 23:28:05 -0700

    Added time utility

---
+++ vira/time_util.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""TimeUtility
+
+A clock.
+
+Authored by Ikechi Akujobi, Matthew Chen, Chris Salguero.
+CS294W, Spring 2016-2017.
+© Stanford University.
+"""
+
+import datetime
+
+class TimeUtility(object):
+	"""Basic class to tell the time"""
+
+	def get_time(self):
+		now = datetime.datetime.now()
+		hour = 12 if now.hour % 12 == 0 else now.hour % 12
+		meridiem = "AM" if now.hour < 12 else "PM"
+		return "The time is %d:%d %s" % (hour, now.minute, meridiem)
+
+def main():
+	tm = TimeUtility()
+	print tm.get_time()
+
+if __name__ == "__main__":
+	main()
